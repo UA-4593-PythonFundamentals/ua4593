e2a658bb3d50796e7274b499c83f55ca1f3beae0
Liubomyr Halamaha <galamagal86@gmail.com>
2025-12-15 19:06:50 +0200

    added lesson01

---
+++ lessons/lesson01/lesson01.py
@@ -0,0 +1,90 @@
+# b = 1 + 21
+# print(b)
+
+
+# b = 1
+# print(type(b), b)
+# b = "Hello, World!"
+# print(type(b), b)
+# b = lambda x: x + 1
+# print(type(b), b, b(1))
+
+# a = 999**999
+# print(len(str(a)))
+# print(a)   
+# import this
+# for i in range(5):
+#     print("Hello, World!")
+#     print(i)
+
+# print("Lesson 01 complete")
+
+
+# my text
+# print("Hello, World!") # my text
+# "sdfdsfgdf"
+# """
+# dsv
+# dsv
+# sd
+# v
+# ds
+# """
+
+# print()
+# sum()
+
+# a # NameError: name 'a' is not defined. assignmentnd usagend usage
+# a = 5
+# print(a)
+
+# import keyword
+# print(len(keyword.kwlist), keyword.kwlist)
+
+# my_name = "John"
+# myAge = 25
+# _my_height = 180
+# print(my_name, myAge, _my_height)
+# # 1st_name = "John"  # SyntaxError: invalid syntax
+# m1_23 = "Test"
+# print(m1_23)
+
+# this_is_a_long_variable_name = "Test"
+# thisIsALongVariableName = "Test2"
+# print(this_is_a_long_variable_name, thisIsALongVariableName)
+
+# a = 10
+# b = 20
+# print(a, b, a == b, a != b, a < b, a > b, a <= b, a >= b)
+# a = 1
+# A = 2
+# print(a, A)
+
+
+# a = 13
+# b = 5
+# print(f"{a+b=}")
+# print(f"{a-b=}")
+# print(f"{a*b=}")
+# print(f"{a/b=}")
+# print(f"{a//b=}")
+# print(f"{a%b=}")
+# print(f"{a**b=}")
+
+age = 10
+name = "John"
+print("my name is " + name + ", I am " + str(age) + " years old.")
+print("="*20)
+print("my name is",
+      name, 
+      ",\n I am", age,
+      "years old.")
+print("="*20)
+print("my name is",name)
+print("I am", age,"years old.")
+print("="*20)
+a = input("Enter your age: ")
+print(type(a), a)
+name = input("Enter your name: ")
+print(type(name), name)
+print("Hello, " + name + "! You are " + a + " years old.")

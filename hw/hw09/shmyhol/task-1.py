da6a0d3571a4bcca049fbaf4115bbc712af5961e
Evhenii Smyhol <evhen.shev@gmail.com>
2026-02-12 09:01:32 +0200

    add textt input frame

--- hw/hw09/shmyhol/task-1.py
+++ hw/hw09/shmyhol/task-1.py
@@ -1,26 +1,30 @@
 import pygame
-
+import sys
+import pygame_gui
 pygame.init()
 
-gameDisplay = pygame.display.set_mode((800, 600))
+WIEDTH, HEIGHT = 800, 600
+SCREEN = pygame.display.set_mode((WIEDTH, HEIGHT))
+pygame.display.set_caption("Task1 game")
 
-pygame.display.set_caption('My Game')
 
-WHITE = (255, 255, 255)
+CLOCK = pygame.time.Clock()
+MANAGRER = pygame_gui.UIManager((WIEDTH, HEIGHT))
 
-clock = pygame.time.Clock()
+TEXT_INPUT = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect((325, 300), (150, 50)), manager=MANAGRER, object_id="#text_input")
+
 done = False
+
 while not done:
-# --- Main event loop
-    for event in pygame.event.get(): # User did something
-        if event.type == pygame.QUIT: # If user clicked close
-            done = True # Flag that we are done so we exit this loop
-# --- Game logic should go here
-# --- Drawing code should go here
-# First, clear the screen to white. Don't put other drawing commands
-# above this, or they will be erased with this command.
-    gameDisplay.fill(WHITE)
-# --- Go ahead and update the screen with what we've drawn.
+    UI_REFRESH_RATE = CLOCK.tick(60)/1000
+    for event in pygame.event.get(): 
+        if event.type == pygame.QUIT: 
+            done = True
+        MANAGRER.process_events(event)
+
+    MANAGRER.update(UI_REFRESH_RATE)
+    
+    SCREEN.fill("white")
+    MANAGRER.draw_ui(SCREEN)
     pygame.display.update()
-# --- Limit to 60 frames per second
-    clock.tick(60)
+    CLOCK.tick(60)
